9ad33fad4d52fac64fc7e79344fba3e8a58dc53c
Guilherme Rossato <guilhermrossato@gmail.com>
2021-08-31 00:11:29 -0300

    Added startup scripts to server
    Such as installation of dependencies and building of the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,69 @@
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+const cp = require("child_process");
+const serverPort = 8081;
+
+if (!fs.existsSync(path.resolve(__dirname, "..", "node_modules"))) {
+  console.log("\n > Installing backend dependencies\n");
+
+  cp.execSync(
+    "npm install --only=production",
+    {
+      cwd: path.resolve(__dirname, ".."),
+      stdio: 'inherit'
+    }
+  );
+}
+
+const frontendPath = path.resolve(__dirname, '..', 'frontend');
+
+if (!fs.existsSync(path.resolve(__dirname, "..", "frontend", "node_modules"))) {
+  console.log("\n > Installing frontend dependencies\n");
+
+  cp.execSync(
+    "npm install --only=production",
+    {
+      cwd: frontendPath,
+      stdio: 'inherit'
+    }
+  );
+}
+
+if (!fs.existsSync(path.resolve(__dirname, "..", "frontend", "build"))) {
+  console.log("\n > Building react frontend\n");
+
+  cp.execSync(
+    "npm run build",
+    {
+      cwd: frontendPath,
+      stdio: 'inherit'
+    }
+  );
+}
+
+console.log("\n > Starting backend server\n");
+
 const express = require("express");
 const { openBrowser } = require("./openBrowser.js");
 const { getVideoMetadata } = require("./VideoModule.js");
-const fs = require("fs").promises;
-const path = require("path");
+
+const frontendBuildPath = path.resolve(__dirname, '..', 'frontend', 'build');
 
 const app = express();
 
-app.use(express.static("frontend/public"));
+app.use(express.static(frontendBuildPath));
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.options("*", function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.end();
-})
+});
 
 app.post("/api/get-video-metadata/", function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   const chunks = [];
 
   req.on("data", function(data) {
@@ -35,18 +84,17 @@ app.post("/api/get-video-metadata/", function(req, res) {
 });
 
 app.post("/api/read/", function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   const chunks = [];
 
   req.on("data", function(data) {
     chunks.push(data);
-  })
+  });
   req.on("end", async function() {
     const targetPath = Buffer.concat(chunks).toString("utf8");
 
     let files;
     try {
-      files = await fs.readdir(targetPath);
+      files = await fsp.readdir(targetPath);
     } catch (err) {
       if (err.code === "ENOENT") {
         return res.status(500).json({
@@ -62,7 +110,7 @@ app.post("/api/read/", function(req, res) {
     const result = await Promise.all(files.map(async (file) => {
       const fullPath = path.resolve(targetPath, file);
       try {
-        const stats = await fs.stat(fullPath);
+        const stats = await fsp.stat(fullPath);
 
         return {
           path: targetPath,
@@ -88,17 +136,22 @@ app.post("/api/read/", function(req, res) {
   });
 });
 
-app.get("/", function(req, res) {
-  res.end("Seja bem vindo");
+app.get("/api/cwd/", function(req, res) {
+  res.json({
+    cwd: process.cwd()
+  });
 });
 
-const listener = app.listen(9090, async function() {
+app.get("/", function(req, res) {
+  const indexHtmlPath = path.resolve(frontendBuildPath, "index.html");
+  res.sendFile(indexHtmlPath);
+});
+
+const listener = app.listen(serverPort, async function() {
   const { address, family, port } = listener.address();
 
   const link = family === "IPv6" ? `http://[${address === "::" ? "::1" : address}]:${port}/` : `http://${address}:${port}/`;
 
-  console.log("Server started at: " + link);
-  /*
   try {
     await openBrowser(link);
   } catch (err) {
@@ -106,5 +159,4 @@ const listener = app.listen(9090, async function() {
     console.log("Open link manually by entering the following on your browser:");
     console.log(link);
   }
-  */
 });
